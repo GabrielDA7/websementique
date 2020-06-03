50d1430053a217b99aa02af5a7dc380f6aa24a26
GabrielDA7 <gabriel.pro.d3@gmail.com>
2020-06-03 14:13:39 +0200

    add graph

--- src/App.js
+++ src/App.js
@@ -1,9 +1,42 @@
 import React, {useState} from 'react';
+import OrgChart from 'react-orgchart';
+import 'react-orgchart/index.css';
 import './App.css';
+
+const MyNodeComponent = ({node}) => {
+    return (
+        <div className="initechNode">{ node.name }</div>
+    );
+};
 
 function App() {
     const [error, setError] = useState(null);
     const [input, setInput] = useState('');
+
+    const data = {
+        name: "Bill Lumbergh",
+        actor: "Gary Cole",
+        children: [
+            {
+                name: "Peter Gibbons",
+                actor: "Ron Livingston",
+                children: [
+                    {
+                        name: "And More!!",
+                        actor: "This is just to show how to build a complex tree with multiple levels of children. Enjoy!"
+                    }
+                ]
+            },
+            {
+                name: "Milton Waddams",
+                actor: "Stephen Root"
+            },
+            {
+                name: "Bob Slydell",
+                actor: "John C. McGi..."
+            },
+        ]
+    };
 
 
     const handleSubmit = (event) => {
@@ -21,6 +54,7 @@ function App() {
                 <input type="text" value={input} onChange={(e) => setInput(e.target.value)}/>
                 <button type="submit">Search</button>
             </form>
+            <OrgChart tree={data} NodeComponent={MyNodeComponent} />
         </main>
     </div>
   );
